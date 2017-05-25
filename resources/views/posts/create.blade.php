0f2a6e5a56e915865c98777465a1a16e1d531a58
Ryan Ebbers <info@ryanebbers.com>
2017-05-25 19:50:37 -0400

    created route for creating a post, created view for creating a post

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,7 @@
+@extends ('layouts.master')
+
+@section ('content')
+	<div class="col-sm-8">
+		<h1>Create a Post</h1>
+	</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -12,4 +12,5 @@
 */
 
 Route::get('/', 'PostsController@index');
-Route::get('/posts/{post}', 'PostsController@show');
+Route::get('/posts/create', 'PostsController@create');
+// Route::get('/posts/{post}', 'PostsController@show');
