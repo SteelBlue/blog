c753e9e4580e7304819c0baf09a95ac5d0a68832
Ryan Ebbers <info@ryanebbers.com>
2017-05-25 17:24:09 -0400

    created query scope for the Task model, query incomplete tasks in the database

--- app/Task.php
+++ app/Task.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    //
+	/**
+	 *	Query Scope for incompleted tasks.
+	 */
+    public function scopeIncomplete($query)
+    {
+    	return $query->where('completed', 0);
+    }
 }
