289b363e290b7a782a004fbfa5195a6f79d5155e
Ryan Ebbers <info@ryanebbers.com>
2017-05-25 19:58:25 -0400

    setup basic form to publish a post

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,6 +2,25 @@
 
 @section ('content')
 	<div class="col-sm-8">
-		<h1>Create a Post</h1>
+
+		<h1>Publish a Post</h1>
+
+		<hr>
+
+		<form>
+			<div class="form-group">
+				<label for="title">Title</label>
+				<input type="text" id="title" class="form-control" name="title" placeholder="Title">
+			</div>
+
+			<div class="form-group">
+				<label for="body">Body</label>
+				<textarea id="body" class="form-control" name="body"></textarea>
+			</div>
+			
+			<button type="submit" class="btn btn-default">Publish</button>
+		</form>
+		<!-- END Post creation form -->
+
 	</div>
 @endsection
