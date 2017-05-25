387afcc2d5a92c89628c8b07df26ac75d16ac4c2
Ryan Ebbers <info@ryanebbers.com>
2017-05-25 17:32:49 -0400

    removed the about page from views and routes.web

--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,4 @@ Route::get('/', function () {
 });
 
 Route::get('/tasks', 'TasksController@index');
-Route::get('/tasks/{task}', 'TasksController@show')
-
-Route::get('/about', function() {
-	return view('about');
-});
+Route::get('/tasks/{task}', 'TasksController@show');
