726ff19bbc00a79090e8ddab7b5e8bc063aad9a9
Ryan Ebbers <info@ryanebbers.com>
2017-05-26 15:41:24 -0400

    fetch all posts and pass them to the index view within the PostsController

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -9,7 +9,10 @@ class PostsController extends Controller
 {
     public function index()
     {
-    	return view('posts.index');
+        // Fetch all posts.
+        $posts = Post::all();
+
+    	return view('posts.index', compact('posts'));
     }
 
     public function show()
