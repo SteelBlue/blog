cec11d21df2cd23f4ad19fd05d5d197f91e3f277
Ryan Ebbers <info@ryanebbers.com>
2017-05-26 18:39:18 -0400

    storing the comment in the database, setup fillable fields in the Comment model, using the addComment method on Post model to create the new comment

--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
+use App\Comment;
 use Illuminate\Http\Request;
 
 class CommentsController extends Controller
 {
-    //
+    public function store(Post $post)
+    {
+    	// Add a comment to a post.
+    	$post->addComment(request('body'));
+
+    	return back();
+    }
 }
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -35,6 +35,8 @@
         <div class="card">
             <div class="card-block">
                 <form method="POST" action="/posts/{{ $post->id }}/comments">
+                    {{ csrf_field() }}
+                    
                     <div class="form-group">
                         <textarea class="form-control" name="body" placeholder="Your comment here..."></textarea>
                     </div>
