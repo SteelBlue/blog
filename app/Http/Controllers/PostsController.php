b72a2f6aa5e82ca0a46e14a93ce577b1489320b9
Ryan Ebbers <info@ryanebbers.com>
2017-05-26 16:00:28 -0400

    using latest() query scope to order the posts desc

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -10,7 +10,7 @@ class PostsController extends Controller
     public function index()
     {
         // Fetch all posts.
-        $posts = Post::all();
+        $posts = Post::latest()->get();
 
     	return view('posts.index', compact('posts'));
     }
