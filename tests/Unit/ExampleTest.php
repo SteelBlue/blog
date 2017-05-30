091537f889fae8400cfb6b56796f8213fb9a0489
Ryan Ebbers <info@ryanebbers.com>
2017-05-30 19:44:35 -0400

    create two posts in the assert test, get those posts, assert there are 2 posts

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -23,10 +23,15 @@ class ExampleTest extends TestCase
     {
     	// Given I have two records in the database that are posts,
     	// and each one is posted a month apart.
+    	$first = factory(Post::class)->create();
+    	$second = factory(Post::class)->create([
+    		'created_at' => \Carbon\Carbon::now()->subMonth()
+    	]);
     	
     	// When I fetch the archives.
-    	Post::archives();
+    	$posts = Post::archives();
     	
     	// Then the response should be in the proper format.
+    	$this->assertCount(2, $posts);
     }
 }
