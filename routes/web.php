33497d36147e82c310be0f710a88e06b773abc19
Ryan Ebbers <info@ryanebbers.com>
2017-05-30 11:03:46 -0400

    added routes for register/login, created RegistrationController, created SessionsController

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,6 @@ Route::post('/posts', 'PostsController@store');
 Route::get('/posts/{post}', 'PostsController@show');
 
 Route::post('/posts/{post}/comments', 'CommentsController@store');
+
+Route::get('/register', 'RegistrationController@create');
+Route::get('/login', 'SessionsController@create');
