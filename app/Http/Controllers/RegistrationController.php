1e867da4d7e2aa75f04beb2ff2612b3c597dcb02
Ryan Ebbers <info@ryanebbers.com>
2017-05-30 12:53:30 -0400

    fixed issue where user password was being stored in the db without bcrypt, fixed the login authentication issue

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -22,9 +22,11 @@ class RegistrationController extends Controller
 		]);
 
     	// Create and save the user.
-    	$user = User::create(
-            request(['name', 'email', 'password'])
-        );
+    	$user = User::create([
+            'name' => request('name'),
+            'email' => request('email'),
+            'password' => bcrypt(request('password'))
+        ]);
 
     	// Sign them in.
     	auth()->login($user);
