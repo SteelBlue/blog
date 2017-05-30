170da9fd4f1597dc04b2303120bb68e15e8481fe
Ryan Ebbers <info@ryanebbers.com>
2017-05-30 14:53:54 -0400

    removed ->toArray() from query

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -20,8 +20,7 @@ class PostsController extends Controller
         // Fetch archives.
         $archives = Post::selectRaw('year(created_at) as year, monthname(created_at) as month, count(*) as published')
             ->groupBy('year', 'month')
-            ->get()
-            ->toArray();
+            ->get();
 
     	return view('posts.index', compact('posts', 'archives'));
     }
