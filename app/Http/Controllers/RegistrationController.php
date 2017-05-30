4fb48d809ec06cdfe222276a346ef5bebc9c99db
Ryan Ebbers <info@ryanebbers.com>
2017-05-30 12:06:29 -0400

    moved the sessions.create view to registration.create because it is for the registration page, started building layout for sign in view

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -9,7 +9,7 @@ class RegistrationController extends Controller
 {
     public function create()
     {
-    	return view('sessions.create');
+    	return view('registration.create');
     }
 
     public function store()
@@ -22,7 +22,9 @@ class RegistrationController extends Controller
 		]);
 
     	// Create and save the user.
-    	$user = User::create(request(['name', 'email', 'password']));
+    	$user = User::create(
+            request(['name', 'email', 'password'])
+        );
 
     	// Sign them in.
     	auth()->login($user);
