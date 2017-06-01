2c8a0d7eaa17eff02ded7e0541bfa43a602c2b28
Ryan Ebbers <info@ryanebbers.com>
2017-06-01 16:19:20 -0400

    including the app.js in the footer, added meta csrf-token to head, fadeOut flash-message after 1.5s

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -7,7 +7,7 @@
 
 require('./bootstrap');
 
-window.Vue = require('vue');
+// window.Vue = require('vue');
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -15,8 +15,16 @@ window.Vue = require('vue');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example', require('./components/Example.vue'));
+// Vue.component('example', require('./components/Example.vue'));
 
-const app = new Vue({
-    el: '#app'
-});
+// const app = new Vue({
+//     el: '#app'
+// });
+
+/**
+ * Hide the flash message if present.
+ */
+
+if ($('#flash-message').length) {
+	$('#flash-message').delay(1500).fadeOut();
+}
