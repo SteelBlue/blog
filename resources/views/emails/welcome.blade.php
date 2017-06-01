93ccd42b8ff4748ce04b2f5381c3305635ced222
Ryan Ebbers <info@ryanebbers.com>
2017-06-01 15:18:03 -0400

    modifying the welcome email markdown

--- resources/views/emails/welcome.blade.php
+++ resources/views/emails/welcome.blade.php
@@ -1,10 +1,14 @@
 @component('mail::message')
 # Introduction
 
-The body of your message.
+Thanks so much for registering!
 
-@component('mail::button', ['url' => ''])
-Button Text
+@component('mail::button', ['url' => 'https://blog.dev'])
+Start Browsing
+@endcomponent
+
+@component('mail::panel')
+Some inspirational quote to go here...
 @endcomponent
 
 Thanks,<br>
